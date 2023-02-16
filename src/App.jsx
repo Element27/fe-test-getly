1cf12fad7d126d89668a378c0f200a9794d354c1
Olamilekan <78114886+Element27@users.noreply.github.com>
2023-02-16 14:20:25 +0100

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>All set and good to go...!!!</h1>
+    </div>
+  )
+}
+
+export default App
